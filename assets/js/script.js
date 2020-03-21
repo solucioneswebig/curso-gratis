dec6b497ef6956ba09e10f324a9ece1ac0174521
solucioneswebig <alexanderr677@gmail.com>
2020-03-20 23:43:10 -0400

    Update 20/03/2020 23:43

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,9 +33,55 @@ if(validaForm_liderBanda()){
 $.post(ruta_servidor+"assets/ajax/enviar.php",$(".form_enviar").serialize(),function(res){
 
      console.log(res)
+        if(res == 1){
+            alert("Su registro fue realizado");
+        }
+     
  });
 }
 });
+
+$("#check_terminos").click(function(){
+
+    if( $('#check_terminos').is(':checked') ) {
+        $(".mensaje").html('');
+    }else{
+
+        $(".mensaje").html('<div class="alert alert-danger">Debe aceptar los terminos y condiciones.</div>');
+        $("#check_terminos").focus();       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+        return false;
+
+    }
+
+})
+
+
+$("#name_form").keyup(function(){
+
+    if($("#name_form").val() ==""){
+    $(".mensaje").html('<div class="alert alert-danger">Su nombre es requerido.</div>');
+    $("#name_form").focus();       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+    return false;
+    }else{
+        $(".mensaje").html('');
+    }
+    
+})
+
+$("#correo_form").keyup(function(){
+
+    if($("#correo_form").val() == ""){
+        $(".mensaje").html('<div class="alert alert-danger">Su correo es requerido.</div>');
+        $("#correo_form").focus();       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+        return false;
+    }else{
+        $(".mensaje").html('');
+    }
+    
+})
+
+
+
 })
 
 
