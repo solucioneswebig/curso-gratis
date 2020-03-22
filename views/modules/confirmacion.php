84b4990af12ff439fe8d174f31fd9eeb8398c8c9
solucioneswebig <alexanderr677@gmail.com>
2020-03-21 20:28:58 -0400

    Update 21/03/2020 20:28

--- views/modules/confirmacion.php
+++ views/modules/confirmacion.php
@@ -56,13 +56,13 @@
 <section class="fondo-principal py-3">
   <div class="container">
      <div class="row justify-content-center">
-      <div class="col-md-3">
-        <img class="img-fluid" src="<?php echo $url_sitio; ?>assets/images/logo-webx-blanco.png">
+      <div class="col-md-3 text-center">
+        <img class="img-fluid logo-confirmacion" src="<?php echo $url_sitio; ?>assets/images/logo-webx-blanco.png">
       </div>
     </div>
     <div class="row justify-content-center pt-4">
       <div class="col-md-6">
-          <h2 class="text-center text-white">COMPLETA EL ÚLTIMO PASO PARA ACCEDER AHORA AL CURSO ONLINE.</h2>   
+          <h2 class="text-center text-white text-completa">COMPLETA EL ÚLTIMO PASO PARA ACCEDER AHORA AL CURSO ONLINE.</h2>   
       </div>
     </div>
   </div>
@@ -73,7 +73,7 @@
     <div class="container">
         <div class="row justify-content-center">
            <div class="col-md-12 py-2 text-center">
-               <img class="imagen-principal" src="<?php echo $url_sitio; ?>assets/images/flechas.png" style="max-width:280px;">
+               <img class="imagen-principal img-fluid" src="<?php echo $url_sitio; ?>assets/images/flechas.png" style="max-width:280px;">
            </div>
         </div>
         <div class="row justify-content-center">
