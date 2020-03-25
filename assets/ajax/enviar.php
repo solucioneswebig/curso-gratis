041198bbeda09c1372ad053bd50e6dc3d5a12572
solucioneswebig <alexanderr677@gmail.com>
2020-03-24 23:27:07 -0400

    Update 24/03/2020 22:36

--- assets/ajax/enviar.php
+++ assets/ajax/enviar.php
@@ -90,7 +90,7 @@ if(isset($_POST["guardar_video"])){
     ];
 
     $where = [
-        "code_registrocurso" => $_POST["code_registrocurso"]
+        "id_registrocurso" => $_POST["id_registrocurso"]
     ];
 
     $actualizar = update('tb_registro_curso',$where,$data);
