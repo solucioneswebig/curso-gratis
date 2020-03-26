f25a391cac2077c2ff034a196a7506042d5ebe51
solucioneswebig <alexanderr677@gmail.com>
2020-03-25 23:36:53 -0400

    Update 25/03/2020 23:36

--- views/modules/video.php
+++ views/modules/video.php
@@ -221,7 +221,7 @@
             </div>
             <div class="row py-4 justify-content-center">
                 <div class="col-md-12 text-center">
-                     <button class="btn boton-comprar">Comprar Ahora</button>
+                     <a class="btn boton-comprar" mp-mode="dftl" href="https://www.mercadopago.com.mx/checkout/v1/redirect?pref_id=267062669-916f87a7-7f93-4434-bde3-5ee72572f748" name="MP-payButton" class='blue-ar-l-rn-none'>Comprar Ahora</a>
                 </div>
             </div>
                  <div class="row justify-content-center">
@@ -304,7 +304,7 @@
             </div>
             <div class="row py-4 justify-content-center">
                 <div class="col-md-12 text-center">
-                     <button class="btn boton-comprar">Comprar Ahora</button>
+                     <a class="btn boton-comprar" mp-mode="dftl" href="https://www.mercadopago.com.mx/checkout/v1/redirect?pref_id=267062669-916f87a7-7f93-4434-bde3-5ee72572f748" name="MP-payButton" class='blue-ar-l-rn-none'>Comprar Ahora</a>
                 </div>
             </div>
                  <div class="row justify-content-center">
@@ -370,7 +370,9 @@ else:
   <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/r-2.2.3/datatables.min.js"></script>
 
   <script src="<?php echo $url_sitio; ?>assets/js/script.js"></script>
-
+  <script type="text/javascript">
+  (function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
+  </script>
 
 </body>
 </html>
