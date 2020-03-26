973a3201a3edc584bab6d7d08da238f065f7c007
solucioneswebig <alexanderr677@gmail.com>
2020-03-26 15:56:27 -0400

    Update 26/03/2020 15:54

--- views/modules/video.php
+++ views/modules/video.php
@@ -1,6 +1,5 @@
 <?php 
 //$url_sitio    = $ruta_second."/views/curso/";
-ob_start();
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -359,7 +358,6 @@ $(document).ready(function(){
 })
 </script>
 <?php
-exit;
 else:
 ?>
 
@@ -384,6 +382,5 @@ else:
   <script type="text/javascript">
 (function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
 </script>
-<?php  ob_get_clean(); ?>
 </body>
 </html>
