9221eae1c0386d5823fab21a547231ac42cc9b35
solucioneswebig <alexanderr677@gmail.com>
2020-03-27 18:27:32 -0400

    Dando estilos al cronometro

--- views/modules/video.php
+++ views/modules/video.php
@@ -1,1 +1,475 @@
-<h1>Video</h1>
+<?php 
+//$url_sitio    = $ruta_second."/views/curso/";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>CURSO GRATIS - 3 Estratégias para Vender en Facebook</title> 
+       
+    <meta name="Description" content="CURSO ONLINE GRATUITO 3 ESTRATÉGIAS PARA PROMOCIONAR MÍ NEGOCIO EN FACEBOOK Y PODER AUMENTAR MIS VENTAS EN 2020."/>
+    <meta name="Keywords" content="CURSO GRATIS DE FACEBOOK, ESTRATEGIAS PARA VENDER EN FACEBOOK, CURSO ONLINE GRATUITO"/>
+    <meta name="author" content="webx.mx">
+    <meta name="owner" content="novapage.net">
+    <meta name="robots" content="noindex, nofollow">
+    <meta property="og:title" content="CURSO GRATIS - 3 Estratégias para Vender en Facebook"/>
+    <meta property="og:description" content="CURSO ONLINE GRATUITO 3 ESTRATÉGIAS PARA PROMOCIONAR MÍ NEGOCIO EN FACEBOOK Y PODER AUMENTAR MIS VENTAS EN 2020."/>
+    <meta property="og:image" content="https://webx.mx//views/curso/assets/images/imagen-principal.png"/>
+    <meta property="og:image:alt" content="PORTADA DE CURSO GRATIS"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="CURSO GRATIS - 3 Estratégias para Vender en Facebook"/>
+    <meta property="og:url" content="https://webx.mx/curso-gratis"/>
+    
+    <link  rel="icon"   href="https://webx.mx/favicon/favicon.png" type="image/png" />
+    
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo $url_sitio; ?>extensions/bootstrap/css/bootstrap.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo $url_sitio; ?>extensions/font-awesome/css/font-awesome.min.css">
+    <!-- Owl Stylesheets -->
+    <link rel="stylesheet" href="<?php echo $url_sitio; ?>extensions/owl/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo $url_sitio; ?>extensions/owl/dist/assets/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="<?php echo $url_sitio; ?>assets/css/style.css">
+  
+
+    
+    <!-- DATATABLE CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/r-2.2.3/datatables.min.css"/>
+ 
+
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    
+    
+    <script src="<?php echo $url_sitio; ?>assets/js/jquery.min.js"></script>
+   
+</head>
+<body>
+
+
+<section class="fondo-principal py-3">
+  <div class="container">
+     <div class="row justify-content-center">
+      <div class="col-md-3 text-center">
+        <img class="img-fluid logo-confirmacion" src="<?php echo $url_sitio; ?>assets/images/logo-webx-blanco.png">
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="py-3 bg-dark">
+    <div class="container">
+        <div class="row py-3 justify-content-center pt-4">
+          <div class="col-md-10">
+              <h2 class="text-center text-white text-completa">LAS 3 ESTRATÉGIAS PARA PROMOCIONAR MÍ NEGOCIO EN <strong class="text-warning">FACEBOOK</strong></h2>
+              <h3 class="text-center text-warning pt-4">Y PODER DUPLICAR TUS VENTAS EN 2020.</h3>   
+          </div>
+        </div>
+        <div class="row py-3 justify-content-center" id="area_video">
+          <div class="col-md-10">
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item"  src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+        <div class="row py-3 justify-content-center">
+           <div class="col-md-12 py-2 text-center">
+               <img class="imagen-principal img-fluid" src="<?php echo $url_sitio; ?>assets/images/flechas.png" style="max-width:350px;">
+               
+           </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="my-2">
+    <div class="container">
+          <div class="row pt-5 justify-content-center">
+                <div class="col-md-8">
+                  <h3 class="text-center tex-mediano"> ¿Ya has visto el CURSO ?
+                  Ha llegado el momento de tomar acción.
+                  </h3>
+               </div>
+          </div>
+
+          <div class="row justify-content-center">
+               <div class="col-md-8">
+                  <h3 class="text-center tex-mediano">Ya no tienes excusas.</h3>
+               </div>
+          </div>
+
+          <div class="row justify-content-center">
+               <div class="col-md-8">
+                  <h4 class="text-center text-pequeno"> Si aún no tienes Página WEB de tu Negocio</h4>
+               </div>
+          </div>
+
+
+          <div class="row justify-content-center">
+               <div class="col-md-8">
+                  <h3 class="text-center text-grande">Que Esperás !!</h3>
+               </div>
+          </div>
+
+          <div class="row justify-content-center">
+               <div class="col-md-8 text-center">
+                  <p class="text-center texto">Adquiere tu Página Web Profesional 100% Funcional e Interactiva, Nosotros haremos todo por ti, </p>
+               </div>
+          </div>
+          
+          
+          <div class="row justify-content-center">
+              <div class="col-md-12 text-center">
+                  <img class="img-fluid" src="<?php echo $url_sitio; ?>assets/images/pagina-video.jpg" style="max-width:880px;">
+              </div>
+          </div>
+        
+    </div>
+</section>
+
+
+<section class="my-5 bg-light">
+    <div class="container">
+          <div class="row pt-5 justify-content-center">
+                <div class="col-md-10 py-2 text-center">
+                      <h4 class="texto text-center">Tu Página Web incluye los recursos necesarios para que pongas en marcha los conocimientos aprendidos en este curso:</h4>
+                </div>
+          </div>
+
+          <div class="row pb-5 justify-content-center">
+            <div class="col-md-3">
+
+              <h5 class="text-danger">Contenido</h5>
+            <ul>
+              <li>Dirección</li>
+              <li>Teléfonos Locales</li>
+              <li>Celulares</li>
+              <li>Horario Interactivo</li>
+              <li>Email</li>
+              <li>Mapa</li>
+
+             </ul>
+              
+              <h5 class="text-danger">Botones</h5>
+             <ul>
+              <li>Whatsapp</li>
+
+             </ul>
+             
+
+             <h5 class="text-danger">Secciones</h5>
+              <ul>
+              <li>Acerca de Nosotros</li>
+              <li>Galería de Imagenes (30)</li>
+              <li>Videos (3)</li>
+              <li>Sección de Comentarios</li>
+              <li>Sección de Promoción</li>
+
+            </ul>
+             
+
+             </div>
+
+             <div class="col-md-3">
+             <h5 class="text-danger"> Posicionamiento Web</h5>
+            <ul>
+              <li>Palabras Claves (SEO)</li>
+              <li>Descripción Personalizada (SEO)</li>
+
+            </ul>
+             
+             <h5 class="text-danger"> Personalización</h5>
+             <ul>
+              <li>Foto de Portada</li>
+              <li>Logo</li>
+              <li>Cambio de Color en Barra y Botones</li>
+            
+            </ul>
+             
+
+            <h5 class="text-danger"> Incluye</h5>
+            <ul>
+              <li>Url con webx.mx/mi-negocio</li>
+              <li>Hospedaje y Mantenimiento por Webx (1año)</li>
+              <li>Envio de Reporte de Visitas cada 4 meses</li>
+              <li>Enlace para Checar Visitas del Mes anterior</li>
+  
+            </ul>
+
+          </div>
+    </div> 
+  </div>
+</section>
+
+
+<section>
+      <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                  <H3 class="text-center tex-mediano">PÁGINA WEB COMPLETA</H3>
+                  <p class="text-pequeno pt-3 text-center">PRECIO ESPECIAL</p>
+                  <h4 class="text-grande text-center">$ 949 <small class="pequeno">IVA INCLUIDO</small></h4>
+                  <h4 class="text-center" style="color:#ca611f">Precio Normal $ 1,315</h4>
+                </div>
+            </div>
+            <div class="row py-4 justify-content-center">
+                <div class="col-md-12 text-center">
+                     <a mp-mode="dftl" href="https://www.mercadopago.com.mx/checkout/v1/redirect?pref_id=267062669-916f87a7-7f93-4434-bde3-5ee72572f748" name="MP-payButton" class='btn boton-comprar click_comprar'>Comprar Ahora</a>
+                </div>
+            </div>
+                 <div class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <img class="img-fluid" src="<?php echo $url_sitio; ?>assets/images/logos-pagos.png" style="max-width:320px;">
+              </div>
+              </div>
+               <div class="row py-4 justify-content-center">
+                    <div class="col-md-6 text-center">
+                        <h4 class="text-grande">Y además lleváte de regalo...</h4>
+                  </div>
+              </div>
+      </div>
+</section>
+
+
+<section class="bg-light">
+      <div class="container">
+            <div class="row pt-5 justify-content-center">
+                  <div class="col-md-8 text-center">
+                     <h4 class="tex-mediano">Una Asesoría Personalizada</h4>
+                  </div>
+            </div>
+
+            <div class="row justify-content-center">
+                  <div class="col-md-8 text-center">
+                      <h4 class="text-pequeno">Un Experto en Marketing Digital se pondrá en contacto contigo para ofrecerte su experiencia y ayudarte a poner en marcha estas Estrategias, con la finalidad que veas resultados a corto plazo.</h4>
+                  </div>
+            </div>
+
+            <div class="row py-5 justify-content-center">
+                <div class="col-md-6 text-center">
+                    <img class="img-fluid" src="<?php echo $url_sitio; ?>assets/images/page-face.jpg">
+                </div>
+            </div>
+
+      </div>
+</section>
+
+
+<section class="my-5">
+      <div class="container">
+            <div class="row pb-5 justify-content-center">
+                  <div class="col-md-8 text-center">
+                     <h4 class="text-grande">Yo de ti, no dejaba escapar esta oportunidad.</h4>
+                  </div>
+            </div>
+
+            <div class="row">
+                  <div class="col-md-6">
+                      <img class="img-fluid" src="<?php echo $url_sitio; ?>assets/images/persona.jpg">
+                  </div>
+                   <div class="col-md-6 pt-4">
+             <p class="parrafo-lista">Mi misión es ayudar al máximo número de Negocios que quieran emprender desarrollando un negocio sólido y exitoso. Para ello he seleccionado aquellos recursos que considero imprescindibles que deben tener una PÁGINA WEB PROFESIONAL, para que tu solo te preocupes por promover tu negocio en FACEBOOK.</p>
+
+             <p class="font-weight-bold pt-3 GothamNarrow-Bold text-name1">ALBERTO RIOS</p>  
+
+             <h5>EMPRENDEDOR Y CEO </h5>
+                  </div>
+            </div>
+      </div>
+</section>
+
+
+<section class="">
+      <div class="container">
+         <div class="row pt-4 justify-content-center">
+                    <div class="col-md-6 text-center">
+                        <h4 class="text-grande">¿Por qué esperar para vender más?</h4>
+                  </div>
+              </div>
+            <br>
+            <br>
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                  <p class="text-pequeno pt-3 text-center">Dale Imagen a tu Negocio en Internet con una</p>   
+                  <H4 class="text-center tex-mediano">PÁGINA WEB COMPLETA</H4>
+                  <p class="text-pequeno pt-3 text-center">PRECIO ESPECIAL</p>
+                  <h4 class="text-grande text-center">$ 949 <small class="pequeno">IVA INCLUIDO</small></h4>
+                  <h4 class="text-center" style="color:#ca611f">Precio Normal $ 1,315</h4>
+                </div>
+            </div>
+            <div class="row py-4 justify-content-center">
+                <div class="col-md-12 text-center">
+                     <a mp-mode="dftl" href="https://www.mercadopago.com.mx/checkout/v1/redirect?pref_id=267062669-916f87a7-7f93-4434-bde3-5ee72572f748" name="MP-payButton" class='btn boton-comprar click_comprar'>Comprar Ahora</a>
+                </div>
+            </div>
+                 <div class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <img class="img-fluid" src="<?php echo $url_sitio; ?>assets/images/logos-pagos.png" style="max-width:320px;">
+              </div>
+              </div>
+                    </div>
+</section>
+
+
+<section class="my-4">
+    <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+                
+          </div>
+      </div>
+  </div>
+</section>
+
+<section class="footer">
+	<div class="container">
+  <div class="row justify-content-center">
+      <div class="col-md-3 text-center">
+        <img class="img-fluid logo-confirmacion" src="<?php echo $url_sitio; ?>assets/images/logo-webx-blanco.png" style="max-width:180px;width:50%;">
+      </div>
+    </div>
+		<div class="row">
+			<div class="col-md-12">
+				<p class="text-center pb-3"><?php echo date("Y"); ?> Todos los derechos reservados. – WEBX.MX</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="fixed-bottom text-center bg-success text-white">
+
+<div id="countdown"></div>
+
+</div>
+
+
+<?php 
+
+
+
+$obtener_dato = select_one("SELECT * FROM tb_registro_curso as curso 
+                            JOIN tb_curso_funnels as funnels on curso.id_registrocurso = funnels.id_registrocurso  and curso.code_registrocurso = '".$rutas[2]."'");
+
+$num_veces = $obtener_dato["nro_veces_visto_funnels"]+1;  
+
+$date_start = $obtener_dato["fecha_primera_vista_funnels"];
+
+if($date_start == "0000-00-00 00:00:00"){
+  $date = new DateTime();
+  $date->modify('+48 hours');
+  $mod_date = $date->format('m/d/Y h:i:s A');
+}else{
+  $date = new DateTime($date_start);
+  $date->modify('+48 hours');
+  $mod_date = $date->format('m/d/Y h:i:s A');
+}
+
+$fecha1 = new DateTime();//fecha inicial
+$fecha2 = new DateTime($mod_date);//fecha de cierre
+
+$intervalo = $fecha1->diff($fecha2);
+
+
+echo $intervalo->format('%H');//00 años 0 meses 0 días 08 horas 0 minutos 0 segundos
+
+?>
+<input type="hidden" name="date_end" id="date_end" value="<?php echo $mod_date; ?>">
+<?php 
+
+if(!$obtener_dato):
+
+$url_video = $ruta_second."/curso-gratis/debe-registrarse/";
+
+
+
+?>
+<script>
+  window.location = "https://webx.mx/curso-gratis/debe-registrarse/"
+</script>
+<?php
+else:
+?>
+
+<form action="" class="form_enviar_video">
+
+<input type="hidden" name="code_registrocurso" id="code_registrocurso" value="<?php echo $obtener_dato['code_registrocurso']; ?>">
+<input type="hidden" name="id_registrocurso" value="<?php echo $obtener_dato['id_registrocurso']; ?>">
+<input type="hidden" name="view_registrocurso" value="1">
+
+<input type="hidden" name="segundos_funnels" value="1" id="contador_pregunta_toda">
+
+<input type="hidden" name="click_comprar_funnels" value="0" id="click_comprar">
+
+<input type="hidden" name="nro_veces_visto_funnels" value="<?php echo $num_veces; ?>">
+
+
+<input type="hidden" name="guardar_video" value="1">
+
+</form>
+<button type="button" id="boton_guardar_video" style="display:none;">Enviar</button>
+<?php endif; ?>
+
+  <script src="<?php echo $url_sitio; ?>extensions/bootstrap/js/bootstrap.js"></script>
+  <script src="<?php echo $url_sitio; ?>extensions/owl/dist/owl.carousel.js"></script>
+  <!-- Datatable -->
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/r-2.2.3/datatables.min.js"></script>
+
+  <script src="<?php echo $url_sitio; ?>assets/js/script.js"></script>
+  <script>
+    $(".click_comprar").click(function(){
+
+      $("#click_comprar").val("1");
+    })
+    
+  </script>
+
+    
+    <script>
+      var date_end = $("#date_end").val();
+
+      console.log(date_end);
+       var end = new Date("<? echo $mod_date; ?>");
+
+        var _second = 1000;
+        var _minute = _second * 60;
+        var _hour = _minute * 60;
+        var _day = _hour * 24;
+        var timer;
+
+        function showRemaining() {
+            var now = new Date();
+            var distance = end - now;
+            if (distance < 0) {
+
+                clearInterval(timer);
+                document.getElementById('countdown').innerHTML = 'EXPIRED!';
+
+                return;
+            }
+            var days = Math.floor(distance / _day);
+            var hours = Math.floor((distance % _day) / _hour);
+            var minutes = Math.floor((distance % _hour) / _minute);
+            var seconds = Math.floor((distance % _minute) / _second);
+            
+            /*
+            document.getElementById('countdown').innerHTML = days + ' dias, ';
+            document.getElementById('countdown').innerHTML += hours + ' horas, ';
+            document.getElementById('countdown').innerHTML += minutes + ' minutos y ';
+            document.getElementById('countdown').innerHTML += seconds + ' segundos';
+            */
+
+            document.getElementById('countdown').innerHTML = "<div class='date'><span class='days'>"+days+" Dias</span> <span class='hours'>"+hours+" Horas</span> <span class='minutes'>"+minutes+" Minutos</span> <span class='seconds'>"+seconds+" Segundos</span></div>"
+        }
+
+        timer = setInterval(showRemaining, 1000);
+    </script>
+
+  <script type="text/javascript">
+(function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
+</script>
+</body>
+</html>
