77c36fc36056fcf2904de970d6cc628848ef7bb8
solucioneswebig <alexanderr677@gmail.com>
2020-03-27 21:25:50 -0400

    Se configuro correo

--- assets/ajax/enviar.php
+++ assets/ajax/enviar.php
@@ -207,9 +207,7 @@ if(isset($_POST["verificar_correo"])){
 
     $buscar_numero_igual = select_one("SELECT * FROM tb_registro_curso where email_registrocurso ='".$_POST['verificar_correo']."'");
 
-    echo $_POST["verificar_correo"];
-
-    
+    var_dump($buscar_numero_igual);
     /*
     if($buscar_numero_igual){
         echo 1;
