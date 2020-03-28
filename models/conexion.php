3ca9004974bd19febb22c4fa327a4df7908cb3b4
solucioneswebig <alexanderr677@gmail.com>
2020-03-28 13:28:01 -0400

    Se configuro correo

--- models/conexion.php
+++ models/conexion.php
@@ -5,22 +5,22 @@
  *
  */
 
-class ConexionOther extends PDO { 
-private $tipo_de_base = 'mysql';
-private $host = 'localhost';
-private $nombre_de_base = 'directo2_IQOS';
-private $usuario = 'directo2_IQOS';
-private $contrasena = '5tbDG*-ycmZ['; 
-public function __construct() {
-  //Sobreescribo el método constructor de la clase PDO.
-  try{
-     parent::__construct("{$this->tipo_de_base}:dbname={$this->nombre_de_base};host={$this->host};charset=utf8", $this->usuario, $this->contrasena);
-  }catch(PDOException $e){
-     echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
-     exit;
-  }
-} 
-} 
+class Conexionother extends PDO { 
+	private $tipo_de_base = 'mysql';
+	private $host = 'localhost';
+	private $nombre_de_base = 'director_directorio';
+	private $usuario = 'director_admon';
+	private $contrasena = 'Qls%jZ5VnKg5'; 
+	public function __construct() {
+	  //Sobreescribo el método constructor de la clase PDO.
+	  try{
+		 parent::__construct("{$this->tipo_de_base}:dbname={$this->nombre_de_base};host={$this->host};charset=utf8", $this->usuario, $this->contrasena);
+	  }catch(PDOException $e){
+		 echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
+		 exit;
+	  }
+	} 
+}      
 
 /**
  * 
